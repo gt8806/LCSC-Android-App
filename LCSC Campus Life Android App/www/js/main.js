ffdb08f1c298926d5f7e2644e82269372f231cb2
sole.dade.lg <sole.dade.lg@gmail.com>
2016-02-16 22:46:20 -0800

    Changed some design features Nina wanted me to

--- LCSC Campus Life Android App/www/js/main.js
+++ LCSC Campus Life Android App/www/js/main.js
@@ -132,7 +132,7 @@ $('#calendar').fullCalendar({
     dayNamesShort: ['', '', '', '', '', '', ''],
     header: {
         left: '',
-        center: 'title',
+        center: '',
         right: ''
     },
 	defaultView:'agendaList',
@@ -230,3 +230,9 @@ function Emergency(){
     $(".eventsources").removeClass("show");
 
 }
+    window.onload = function () {
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];;
+        var date = new Date();
+
+        document.getElementById('date').innerHTML = months[date.getMonth()] + ' ' + date.getFullYear();
+    };
