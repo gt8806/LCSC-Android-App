41f6300afff67e4413823ad4a90dff8728c4d7b4
Kevin Karel <krkarel@lcmail.lcsc.edu>
2016-02-26 11:52:03 -0800

    Changed header background collor filter icon color and lcsc logo

--- LCSC Campus Life Android App/www/js/main.js
+++ LCSC Campus Life Android App/www/js/main.js
@@ -203,9 +203,3 @@ function Emergency(){
     $(".eventsources").removeClass("show");
 
 }
-    window.onload = function () {
-        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        var date = new Date();
-
-        document.getElementById('date').innerHTML = months[date.getMonth()] + ' ' + date.getFullYear();
-    };
