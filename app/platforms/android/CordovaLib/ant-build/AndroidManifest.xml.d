96a44ee4eda67d0c46c08a291ae386e79467e120
Kevin Karel <krkarel@lcmail.lcsc.edu>
2016-03-01 01:08:48 -0800

    Current state of things properly signed and built, with no broken dependencies or extra garbage.

---
+++ app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/krkarel/Downloads/repos/android/LCSC-Android-App/app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/krkarel/Downloads/repos/android/LCSC-Android-App/app/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ app/platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/home/krkarel/Downloads/repos/android/LCSC-Android-App/app/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
+/home/krkarel/Downloads/repos/android/LCSC-Android-App/app/platforms/android/ant-build/CordovaApp.ap_ \
+/home/krkarel/Downloads/repos/android/LCSC-Android-App/app/platforms/android/ant-build/classes.dex \
+/home/krkarel/Downloads/repos/android/LCSC-Android-App/app/platforms/android/CordovaLib/ant-build/classes.jar \
