e5086888cfc1bd6140b6348d1d0ae2856646eb18
Kevin Karel <krkarel@lcmail.lcsc.edu>
2016-03-01 01:08:48 -0800

    Current state of things properly signed and built, with no broken dependencies or extra garbage.

---
+++ app/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\kmdimond\Desktop\app\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\Users\kmdimond\Desktop\app\platforms\android\ant-build\CordovaApp.ap_ \
+C:\Users\kmdimond\Desktop\app\platforms\android\ant-build\classes.dex \
+C:\Users\kmdimond\Desktop\app\platforms\android\CordovaLib\ant-build\classes.jar \
